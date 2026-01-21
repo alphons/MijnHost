04ad8e68f37bc3872e6cea93d2327ea660f3b8d6
alphons <alphons@heijden.com>
2026-01-21 23:11:42 +0100

    ok

--- MijnHost/CheckAllHelper.cs
+++ MijnHost/CheckAllHelper.cs
@@ -6,31 +6,31 @@ namespace MijnHost;
 
 public class CheckAllHelper
 {
+	private static readonly string ACME = "acme.certservice.net."; // must end in .
 	public static async Task CheckAlAsync(DnsApiClient client)
 	{
+		if (!ACME.EndsWith('.'))
+			return;
 		DnsResponse<DnsDomains> dnsResponse = await client.GetDomainsAsync();
 		foreach (DnsDomain dom in dnsResponse.Data.Domains)
 		{
 			DnsResponse<DnsRecords> dnsRecords = await client.GetDomainRecordsAsync(dom.Domain);
 
 			DnsRecord? challenge = dnsRecords.Data.Records.FirstOrDefault(x => x.Name.StartsWith("_acme-challenge"));
-			if (challenge != null)
+			if (challenge != null && challenge.Value.EndsWith(ACME))
+				continue;
+
+			DnsApiResponse? result = await client.PatchDnsRecordAsync(
+				dom.Domain, new DnsRecord($"_acme-challenge", "CNAME", $"{dom.Domain}.{ACME}", 60));
+			if (result.Status == 200)
 			{
-				Console.WriteLine($"{challenge.Name} -> {challenge.Value}");
+				Console.WriteLine($"Added _acme-challenge CNAME {dom.Domain}.{ACME}");
 			}
 			else
 			{
-				DnsApiResponse? result = await client.PatchDnsRecordAsync(
-					dom.Domain, new DnsRecord($"_acme-challenge", "CNAME", $"{dom.Domain}.acme.certservice.nl.", 60));
-				if(result.Status == 200)
-				{
-					Console.WriteLine($"Added _acme-challenge for {dom.Domain}");
-				}
-				else
-				{
-					Console.WriteLine($"Failed to add _acme-challenge for {dom.Domain}: {result.StatusDescription}");
-				}
+				Console.WriteLine($"Failed to add _acme-challenge for {dom.Domain}: {result.StatusDescription}");
 			}
 		}
 	}
 }
+
